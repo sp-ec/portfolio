e8214067bb7d8326ed0c67a65671b9c5f049ed7d
Spectrobyte <48649161+Spectromeda@users.noreply.github.com>
2024-09-03 13:09:39 -0400

    disabled cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mysql = require('mysql')
-const cors = require('cors')
+//const cors = require('cors')
 
 const app = express()
-app.use(cors())
+//app.use(cors())
 
 const db = mysql.createConnection({
     host: 'localhost',
